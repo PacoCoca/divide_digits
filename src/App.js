8cccae8839cbb5bc1977ae27575b204790e0ddc4
PacoCoca <pacococacruz@gmail.com>
2020-11-12 18:26:56 +0100

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function convertToDigigts(number) {
+  let digits = [];
+
+  const negative = number < 0;
+  number = negative ? -number : number;
+  while (number) {
+    const digit = number % 10;
+    number = (number - digit) / 10;
+    digits.push(digit);
+  }
+  digits = digits.reverse();
+
+  if (negative)
+    digits[0] = digits[0] * -1;
+
+  return digits;
+}
+
+function App(props) {
+  const [number, setNumber] = useState(0);
+  const digits = convertToDigigts(number);
+
+  function handleChange(event) {
+    setNumber(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <p>Insert a number:</p>
+      <input
+        type='number'
+        onChange={handleChange}
+      />
+      {
+        digits.length > 0 &&
+        <p>The digits are:</p>
+      }
+      {
+        digits.map((value, index) => {
+          const add = (index === digits.length - 1) ? '' : ' , ';
+          return `${value}${add}`;
+        })
+      }
     </div>
   );
 }
